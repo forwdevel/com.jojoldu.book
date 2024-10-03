81880a9d6f75617c56fa4523b742e38e5ae4eddd
forwdevel <forwdevel@gmail.com>
2024-10-03 20:43:23 +0900

    SR-2021001-0001-W01 C005 금일 개발완료 커밋

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,10 +2,16 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties("spring.config.location=classpath:/application.yml")
+                .run(args);
     }
 }
